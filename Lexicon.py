7a1410a37a9de7c0f92cc1bf061549e47ad4da55
smiya10 <sydsaki@gmail.com>
2015-06-08 14:59:56 -0400

    Address error handling and exiting.

--- Lexicon.py
+++ Lexicon.py
@@ -7,7 +7,7 @@ def file_opener(file_name):
         file_handle = open(file_name)
     except FileNotFoundError:
         print('File not found, please make sure the file is in your directory')
-        file_handle = None
+        file_handle = 'swallow'
     return file_handle
 
 
@@ -31,6 +31,7 @@ def clean_words(words):
     new_vocab = []
     for word in words:
         word = re.sub('\([A-Z]*[0-9]*[a-z]*\)', ' ', word)
+        word = re.sub('\[[A-Z]*[0-9]*[a-z]*\]', ' ', word)
         word = re.sub(r'[!$#%*\(\)\{\}\[\]\+\"=|/:;~`\\]', ' ', word)
         word = word.lower()
         word = word.strip()
@@ -64,11 +65,17 @@ def printer(data, values=False):
 
     return counter
 
+def input_screen():
+    f_name = input("Enter a file name: ", )
+    if f_name == 'quit': exit()    
+    f_hand = file_opener(f_name)
+    if f_hand == 'swallow': input_screen():
+    else: return f_hand, f_name
 
-f_name = input("Enter a file name: ", )
-f_hand = file_opener(f_name)
+f_hand, f_name = input_screen()
 vocab = word_list(file_reader_lines(f_hand))
 fin_vocab = vocab_cleanse(vocab)
+
 print("The file " + f_name + " has a vocabulary of the words: ")
 count = printer(fin_vocab, values=True)
 print("There are " + str(count) + " unique word in " + f_name)
